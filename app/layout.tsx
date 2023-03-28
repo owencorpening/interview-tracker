e49334aaaf2276a0ec1fb965aa30a7405b80a5fb
Owen Corpening <owencorpening@yahoo.com>
2023-03-28 14:11:13 -0500

    changed text color to red

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import '/dist/output.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </head>
       <body>{children}</body>
     </html>
   )
 }
+
+/*
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
+</head>
+*/
